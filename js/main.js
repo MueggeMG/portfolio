cd2dbb84a8751284dec69f8ecf23fb89a1e982bb
Muegge <marcelgraf79@gmail.com>
2026-03-24 21:34:27 +0100

    Added base.js + small fixes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-// Navigationsoverlay beim Scrollen
-window.addEventListener('scroll', () => {
-  const nav = document.querySelector('nav');
-
-  if (window.scrollY > 60) {
-    nav.classList.add('scrolled');
-  } else {
-    nav.classList.remove('scrolled');
-  }
-});
-
 // Scroll-Animation (Sanftes Einblenden)
 const revealElements = document.querySelectorAll('section');
 
@@ -25,31 +14,3 @@ const observer = new IntersectionObserver(
 revealElements.forEach((el) => {
   observer.observe(el);
 });
-
-// Custom Cursor
-const cursor = document.getElementById('cursor');
-let mx = 0,
-  my = 0,
-  cx = 0,
-  cy = 0;
-
-document.addEventListener('mousemove', (e) => {
-  mx = e.clientX;
-  my = e.clientY;
-});
-
-function animateCursor() {
-  cx += (mx - cx) * 0.12;
-  cy += (my - cy) * 0.12;
-  cursor.style.transform = `translate(${cx - 4}px, ${cy - 4}px)`;
-  requestAnimationFrame(animateCursor);
-}
-
-animateCursor();
-
-document
-  .querySelectorAll('a, button, .project-item, .skill-card, .stat-box')
-  .forEach((el) => {
-    el.addEventListener('mouseenter', () => cursor.classList.add('grow'));
-    el.addEventListener('mouseleave', () => cursor.classList.remove('grow'));
-  });
